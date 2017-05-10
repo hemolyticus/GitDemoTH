dde3405b8f7bf84bc4d0b31abf28c90b60bf5713
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-11 10:37:51 +1200

    Committing change

--- gitDemoTH/ViewController.swift
+++ gitDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let userName = "dummyUserChanged"
+    let userName = "dummyUserChangedModified"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
